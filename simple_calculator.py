062422e2d43fa5bea593089737c6a4dd098aca12
Humam Al Labib <humam.alabib@gmail.com>
2026-02-09 16:46:43 +0700

    change variable

--- simple_calculator.py
+++ simple_calculator.py
@@ -9,8 +9,8 @@ class Calculator:
         return a / b
     def modulo(self, a, b):
         return a % b
-    def power(self, a, b):
-        return a ** b
+    def power(self, x, y):
+        return x ** y
 if __name__ == "__main__":
     calc = Calculator()
     print("Addition: ", calc.add(10, 5))
