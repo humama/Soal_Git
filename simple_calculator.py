86b960653b9517aea96de646c77c6df63c9681e0
Humam Al Labib <humam.alabib@gmail.com>
2026-02-09 16:43:50 +0700

    change variable

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,8 +1,8 @@
 class Calculator:
     def add(self, a, b):
         return a + b
-    def subtract(self, a, b):
-        return a - b
+    def subtract(self, x, y):
+        return x - y
     def multiply(self, a, b):
         return a * b
     def divide(self, a, b):
